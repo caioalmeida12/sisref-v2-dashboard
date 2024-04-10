a64906f051f357c8e013dbc7b391dad9007f80b1
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-04-10 10:35:40 -0300

    feat(elementos): implementei o componente slider

---
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/Slider.tsx
@@ -0,0 +1,15 @@
+import { IconeSeta } from "../basicos/icones/IconeSeta";
+
+interface SliderProps {
+    texto: string;
+}
+
+export const Slider = ({ texto }: SliderProps) => {
+    return (
+        <div className="p-4 bg-cinza-600 text-center text-branco-400 font-bold rounded justify-between flex items-center">
+            <IconeSeta fill="fill-branco-400" direcao="esquerda"/>
+            {texto}
+            <IconeSeta fill="fill-branco-400" direcao="direita"/>
+        </div>
+    );
+}
