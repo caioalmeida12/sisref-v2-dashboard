ccdd3d2ff740bb403a7dc08bd1f917131f54c271
LuanF11 <luaninfo09@gmail.com>
2024-04-15 12:18:00 -0300

    feat(refeicaoautorizada): implementado o componente de RefeicaoAutorizada

--- sisref-v2-dashboard-client/src/app/elementos/componentes/RefeicaoAutorizada.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/RefeicaoAutorizada.tsx
@@ -1,26 +1,28 @@
-import React from "react";
 import { NomeDaRefeicao } from "../basicos/NomeDaRefeicao";
+import { HorarioDaRefeicao, Horarios } from "../basicos/HorarioDaRefeicao";
 
 interface RefeicaoAutorizadaProps {
     variante: keyof typeof horarioPorVariante;
+    horarios: Horarios;
+    dias: string[];
 }
 
 const horarioPorVariante = {
-    "manha": "9:20 - 9:50",
-    "almoco": "11:20 - 13:00",
-    "tarde": "14:45 - 15:35",
-    "noite": "20:00 - 20:20",
+    "manha": { qtdTimeReservationEnd: 9, qtdTimeReservationStart: 20, timeEnd: "9:50", timeStart: "9:20" },
+    "almoco": { qtdTimeReservationEnd: 11, qtdTimeReservationStart: 20, timeEnd: "13:00", timeStart: "11:20" },
+    "tarde": { qtdTimeReservationEnd: 14, qtdTimeReservationStart: 45, timeEnd: "15:35", timeStart: "14:45" },
+    "noite": { qtdTimeReservationEnd: 20, qtdTimeReservationStart: 0, timeEnd: "20:20", timeStart: "20:00" },
 } as const;
 
-export const RefeicaoAutorizada = ({ variante}: RefeicaoAutorizadaProps) => {
+export const RefeicaoAutorizada = ({ variante, dias }: RefeicaoAutorizadaProps) => {
     return (
         <div className="flex p-4 flex-col gap-4 rounded border-cinza-600 text-preto-400">
             <div className="flex justify-between items-start">
                 <NomeDaRefeicao variante={variante}/>  
-                <span className="flex justify-center items-center text-cinza-600">{horarioPorVariante[variante]}</span>
+                <HorarioDaRefeicao variante="horario" horarios={horarioPorVariante[variante]} />
             </div>
             <div>
-                <b>Dias autorizados:</b> <span>segunda-feira, terça-feira, quarta-feira, quinta-feira, sexta-feira</span>
+                <b>Dias autorizados:</b> <span>{dias.join(', ')}</span>
             </div>
         </div>
     )
