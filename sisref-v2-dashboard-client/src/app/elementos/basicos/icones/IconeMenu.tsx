d05f1f43c6ee2cc2c30cc81edbcd2c5295723661
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-04-15 11:49:56 -0300

    feat(elementos): implementei o drawer lateral para a navbar

--- sisref-v2-dashboard-client/src/app/elementos/basicos/icones/IconeMenu.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/basicos/icones/IconeMenu.tsx
@@ -1,6 +1,10 @@
-export const IconeMenu = () => {
+interface IconeMenuProps extends React.SVGProps<SVGSVGElement> {
+
+}
+
+export const IconeMenu = ({...rest}: IconeMenuProps) => {
     return (
-        <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest} className="cursor-pointer">
             <path d="M0 0H18V2H0V0ZM0 5H18V7H0V5ZM0 10H18V12H0V10Z" fill="white" />
         </svg>
     )
---
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/MenuDrawer.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { IconeMenu } from "../basicos/icones/IconeMenu";
+import { INavbarProps } from "../interfaces/INavbarProps";
+import { useState } from "react";
+import { IconeFechar } from "../basicos/icones/IconeFechar";
+import { MenuDropdown } from "./MenuDropdown";
+import { MenuNavItem } from "./MenuNavItem";
+
+export const MenuDrawer = ({ navItems }: INavbarProps) => {
+    const [aberto, setAberto] = useState(false);
+
+    const IconeFecharSeAberto = aberto && (
+        <div className="flex justify-end px-6 py-4">
+            <IconeFechar onClick={() => setAberto(false)} />
+        </div>
+    )
+
+    const ConteudoSeAberto = aberto && (
+        <div className={`flex flex-col gap-y-4 px-4 transition-all 
+                         ${aberto? "opacity-100" : "opacity-0"}
+                         md:px-[3em]
+                        `}>
+            {
+                navItems.map((navItem, index) => (
+                    <li key={index} className="flex items-center gap-x-2 cursor-pointer">
+                        {navItem.isDropdown ? (
+                            <MenuDropdown titulo={navItem.titulo} itens={navItem.itens} isDropdown={true} />
+                        ) : (
+                            <MenuNavItem titulo={navItem.titulo} rota={navItem.rota} />
+                        )}
+                    </li>
+                ))
+            }
+        </div>
+    )
+
+    return (
+        <>
+            <IconeMenu onClick={() => setAberto(true)} />
+            <div
+                className={`
+                        flex flex-col gap-y-4 fixed z-20 bg-verde-400 transition-all
+                        w-0 h-full top-0 left-0
+                        ${aberto ? "w-60 lg:w-80" : "w-0"}
+                        `}>
+                {IconeFecharSeAberto}
+                {ConteudoSeAberto}
+            </div>
+        </>
+    );
+}
