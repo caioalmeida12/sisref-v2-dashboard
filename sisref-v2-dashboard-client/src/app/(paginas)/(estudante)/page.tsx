c9f0ff7ff9e41361f2f541445a86c5a6b8f7510e
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-04-22 11:08:00 -0300

    feat(pages): limpei os elementos desnecessários da página de estudante

--- sisref-v2-dashboard-client/src/app/(paginas)/(estudante)/page.tsx
+++ sisref-v2-dashboard-client/src/app/(paginas)/(estudante)/page.tsx
@@ -49,50 +49,6 @@ export default async function Home() {
         <RefeicoesPorDia />
         <RestricoesAlimentares />
         <RefeicoesAutorizadas />
-
-        <Secao>
-          <CabeçalhoDeSecao titulo="Texto de cabeçalho de seção" />
-          <CabecalhoPrincipal titulo="Texto de cabeçalho principal" />
-          <h1>Olá, mundo!</h1>
-          <CampoDeSecao titulo="Texto de campo de seção" complemento="Texto de complemento de campo de seção" variante="horizontal" />
-          <CampoDeSecao titulo="Texto de campo de seção" complemento="Texto de complemento de campo de seção" variante="vertical" />
-          <CampoDeSecao titulo="Texto de campo de seção" complemento="Texto de complemento de campo de seção" variante="horizontal-com-badge" corDaBadge="bg-verde-400" />
-
-          <Botao texto="Adicionar" variante="adicionar" />
-          <Botao texto="Editar" variante="editar" />
-          <Botao texto="Ocultar" variante="ocultar" />
-          <Botao texto="Remover" variante="remover" />
-
-          <Slider texto='1/10' />
-
-          <StatusDaRefeicao texto='Utilizado' cor='verde-300' icone='circulo-check' />
-          <StatusDaRefeicao texto='Reservado' cor='verde-300' icone='circulo-check' />
-          <StatusDaRefeicao texto='Disponível' cor='cinza-600' icone='circulo-check' />
-          <StatusDaRefeicao texto='Justificado' cor='azul-400' icone='circulo-check' />
-          <StatusDaRefeicao texto='Encerrado' cor='cinza-600' icone='circulo-x' />
-          <StatusDaRefeicao texto='Cancelado' cor='vermelho-400' icone='tag-x' />
-          <StatusDaRefeicao texto='Não utilizado' cor='amarelo-200' icone='circulo-check' />
-          <StatusDaRefeicao texto='Bloqueado' cor='amarelo-200' icone='cadeado' />
-
-          <NomeDaRefeicao variante='manha' />
-          <NomeDaRefeicao variante='almoco' />
-          <NomeDaRefeicao variante='tarde' />
-          <NomeDaRefeicao variante='noite' />
-
-          <RestricaoAlimentar texto='Glúten' />
-
-          <HorarioDaRefeicao variante='data' data='12/04/2024' />
-          <HorarioDaRefeicao variante='horario' horarios={{ qtdTimeReservationEnd: 0, qtdTimeReservationStart: 0, timeEnd: '12:00', timeStart: '11:00' }} />
-          <HorarioDaRefeicao variante='horario-e-data' horarios={{ qtdTimeReservationEnd: 0, qtdTimeReservationStart: 0, timeEnd: '12:00', timeStart: '11:00' }} data='12/04/2024' />
-
-          <Aviso titulo='Texto de aviso' texto="Devido à queda da internet no campus, todas as reservas de alimentação, exceto a do lanche da noite, serão feitas de maneira presencial na recepção." />
-
-
-          <SecaoApenasTexto titulo="Texto de seção apenas com texto" texto="Texto de conteúdo de seção apenas com texto" />
-        </Secao>
-
-
-
       </main>
       <Footer />
     </>
