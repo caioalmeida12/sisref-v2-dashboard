0f024b46026d9e9f6f586608fc3b5ae5468685ab
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2024-04-30 10:44:14 -0300

    feat(refeicoesPorDia): adicionei um tooltip ao modulo refeicoes para informar quando não houver refeicoes

--- sisref-v2-dashboard-client/src/app/elementos/componentes/Slider.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/Slider.tsx
@@ -6,17 +6,18 @@ import { IconeSeta } from "@elementos/basicos/icones/IconeSeta";
 
 interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {
     texto: string;
+    tooltip?: React.ReactNode;
     onNext?: () => void;
     onPrevious?: () => void;
 }
 
-export const Slider = ({ texto, onPrevious, onNext, ...rest }: SliderProps) => {
+export const Slider = ({ texto, onPrevious, onNext, tooltip, ...rest }: SliderProps) => {
     return (
         <div className={`${rest.className} p-4 bg-cinza-600 text-center text-branco-400 font-bold rounded justify-between flex items-center`}>
             <button onClick={onPrevious}>
                 <IconeSeta fill="fill-branco-400" direcao="esquerda" />
             </button>
-            {texto}
+            <span className='flex justify-center gap-x-2'>{texto}{tooltip}</span>
             <button onClick={onNext}>
                 <IconeSeta fill="fill-branco-400" direcao="direita" />
             </button>
