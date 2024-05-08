4f9be2bd7b1c0f00cb3b6a5eb2ce114bad37c480
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2024-05-08 11:20:29 -0300

    feat(historico): implementei o historico de refeicoes

---
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/RefeicaoDoHistorico.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { HorarioDaRefeicao } from "@elementos/basicos/HorarioDaRefeicao";
+import { NomeDaRefeicao } from "@elementos/basicos/NomeDaRefeicao";
+import { Secao } from "@elementos/basicos/Secao";
+import { StatusDaRefeicao } from "@elementos/basicos/StatusDaRefeicao";
+import { IRefeicaoDoHistorico } from "../interfaces/IRefeicaoDoDoHistorico";
+import { DatasHelper } from "@/app/lib/elementos/DatasHelper";
+
+const varianteNomeRefeicaoPorTurno = {
+    1: "manha",
+    2: "almoco",
+    3: "tarde",
+    4: "noite"
+} as const;
+
+const elementoStatusRefeicaoPorTextoStatusRefeicao = {
+    "a-ser-utilizado": <StatusDaRefeicao cor="azul-400" icone="circulo-check" texto="A ser utilizado" />,
+    "nao-utilizado": <StatusDaRefeicao cor="amarelo-200" icone="circulo-x" texto="Não utilizado" />,
+    "justificado": <StatusDaRefeicao cor="azul-400" icone="circulo-check" texto="Justificado" />,
+    "cancelado": <StatusDaRefeicao cor="vermelho-400" icone="tag-x" texto="Cancelado" />,
+    "utilizado": <StatusDaRefeicao cor="verde-300" icone="circulo-check" texto="Utilizado" />,
+} as const;
+
+/**
+ * Converte a descrição do cardápio em um array de strings.
+ * @param descricao - A descrição do cardápio.
+ * @returns O array de strings.
+ */
+const descricaoCardapioParaArrayStrings = (descricao: string) => {
+    return descricao.split(/[;+]/).filter(naoVazio => naoVazio)
+}
+
+export const RefeicaoDoHistorico = (props: IRefeicaoDoHistorico) => {
+    return (
+        <Secao className="flex flex-col gap-4">
+            <div className="flex justify-between">
+                <NomeDaRefeicao variante={varianteNomeRefeicaoPorTurno[props.turno]} />
+                {elementoStatusRefeicaoPorTextoStatusRefeicao[props.status]}
+            </div>
+            <HorarioDaRefeicao 
+            variante="horario-e-data"
+            data={DatasHelper.converterParaFormatoBrasileiro(props.cardapio.date)} 
+            horarios={{
+                qtdTimeReservationEnd: props.refeicao.qtdTimeReservationEnd,
+                qtdTimeReservationStart: props.refeicao.qtdTimeReservationStart,
+                timeEnd: DatasHelper.removerSegundosDoHorario(props.refeicao.timeEnd),
+                timeStart: DatasHelper.removerSegundosDoHorario(props.refeicao.timeStart)
+            }}
+            />
+            <p className="leading-6">
+                {descricaoCardapioParaArrayStrings(props.cardapio.description).map((descricao, index) => (
+                    <React.Fragment key={index}>
+                        <span>
+                            {descricao}
+                        </span>
+                        <br />
+                    </React.Fragment>
+                ))}
+            </p>
+        </Secao>
+    )
+}
---
+++ sisref-v2-dashboard-client/src/app/elementos/interfaces/IRefeicaoDoDoHistorico.ts
@@ -0,0 +1,20 @@
+export interface IRefeicaoDoHistorico {
+    turno: 1 | 2 | 3 | 4;
+    cardapio: {
+        id: number;
+        date: string;
+        description: string;
+        campus_id: number;
+        meal_id: number;
+    };
+    refeicao: {
+        id: number;
+        description: string;
+        timeEnd: string;
+        timeStart: string;
+        campus_id: number;
+        qtdTimeReservationEnd: number;
+        qtdTimeReservationStart: number;
+    };
+    status: "a-ser-utilizado" | "utilizado" | "cancelado" | "nao-utilizado";
+}
