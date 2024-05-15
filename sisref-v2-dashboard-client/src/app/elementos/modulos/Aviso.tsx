d1de195847df9d27fae745d2fd5ec8498d8afc68
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-05-15 11:20:00 -0300

    refactor(aviso): implementei a utilizacao do radix no componente

--- sisref-v2-dashboard-client/src/app/elementos/modulos/Aviso.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/modulos/Aviso.tsx
@@ -15,28 +15,30 @@ interface AvisoProps {
 }
 
 export const Aviso = ({ titulo, texto, textoBotao }: AvisoProps) => {
-    const conteudoEstaVisivel = useRef(false);
     const elementoPai = useRef<HTMLDivElement>(null);
     const elementoConteudo = useRef<HTMLParagraphElement>(null);
 
     const alterarVisibilidade = (elemento: HTMLElement | null) => {
         if (!elemento) return;
         
-        conteudoEstaVisivel.current = !conteudoEstaVisivel.current;
-        elementoPai.current?.setAttribute('data-aberto', conteudoEstaVisivel.current.toString());
+        const estaAberto = elementoPai.current?.getAttribute('data-state') === 'open';
+        const proximoEstado = !estaAberto;
+        const proximoEstadoString = proximoEstado ? 'open' : 'closed';
+
+        elementoPai.current?.setAttribute('data-state', proximoEstadoString);
 
         toggle(elemento);
     }
 
     return (
         
-        <div className="bg-vermelho-400 text-branco-400 p-4 rounded gap-x-2 gap-y-4 group col-left" ref={elementoPai} data-aberto={conteudoEstaVisivel}>
+        <div className="bg-vermelho-400 text-branco-400 p-4 rounded gap-x-2 gap-y-4 group col-left peer" ref={elementoPai} data-state="open">
             <div className='flex justify-between items-center'>
                 <h2 className="font-bold text-branco-400 flex gap-x-2 items-center"><IconeAviso />{titulo}</h2>
-                <IconeDropdown variante='circulo' className='rotate-180 group-data-[aberto=true]:rotate-0 transition' onClick={() => alterarVisibilidade(elementoConteudo.current)} />
+                <IconeDropdown variante='circulo' onClick={() => alterarVisibilidade(elementoConteudo.current)} />
             </div>
-            <div className='group-data-[aberto]:mt-0 transition-all duration-300' ref={elementoConteudo}>
-                <p className='mb-4 mt-4 transition-opacity opacity-0 group-data-[aberto]:opacity-100'>
+            <div className='transition-all duration-300' ref={elementoConteudo}>
+                <p className='mb-4 mt-4 transition-opacity opacity-0 group-data-[state=open]:opacity-100'>
                     {texto}
                 </p>
                 <Botao texto={textoBotao || "Entendido"} variante="ocultar" onClick={() => alterarVisibilidade(elementoConteudo.current)} />
