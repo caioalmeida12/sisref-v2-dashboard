df66fcf3f80a92f0ef518fe22162d91d0c14ec2c
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-05-23 15:50:00 -0300

    refactor(login): substitui a mui pela radix ui

--- sisref-v2-dashboard-client/src/app/elementos/modulos/Login.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/modulos/Login.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 
-import { TextField } from '@mui/material';
 import Image from 'next/image';
 import sisrefLogo from '@/app/elementos/assets/img/sisrefLogo.png';
 import { Botao } from '../basicos/Botao';
@@ -10,10 +9,12 @@ import { fetchLoginAPI } from '@/app/actions/fetchLoginApi';
 import { useSearchParams } from 'next/navigation';
 import { useFormStatus } from 'react-dom';
 
+import * as Form from '@radix-ui/react-form';
+
 const MensagemErro = ({ texto }: { texto: string | null }) => {
     const { pending } = useFormStatus()
 
-    if (pending) return null
+    if (pending || !texto) return null
 
     return (
         <div className="text-red-500 text-sm text-vermelho-400 text-center">{texto}</div>
@@ -34,51 +35,27 @@ export const Login = () => {
     const params = useSearchParams()
 
     return (
-        <>
-            <div className="flex flex-col items-center gap-14 col-span-2">
-                <Image src={sisrefLogo} alt="Sisref" />
-                <div className="flex flex-col">
-                    <div className="text-center text-lg font-bold text-green-500 leading-7">Restaurante Universitário</div>
-                    <div className="text-center text-base font-normal text-black leading-7">IFCE Campus Cedro</div>
-                </div>
-                <form className="flex flex-col gap-4" action={fetchLoginAPI}>
-                    <div className="flex items-center p-4 rounded w-9/10">
-                        <TextField
-                            type="email"
-                            name="email"
-                            id="Email"
-                            fullWidth
-                            variant="outlined"
-                            label="Email"
-                        />
-                    </div>
-                    <div className="flex items-center p-4 rounded w-9/10">
-                        <TextField
-                            type="password"
-                            name="password"
-                            id="Password"
-                            fullWidth
-                            variant="outlined"
-                            label="Senha"
-                        />
-                    </div>
-                    <div className="flex gap-20">
-                        <div className="flex items-start gap-2">
-                            <input
-                                type="checkbox"
-                                name="remember"
-                                id="remember"
-                                className="rounded border-2 border-black"
-                            />
-                            <label htmlFor="remember">Lembre-se de mim</label>
-                        </div>
-                        <a href="#" className="text-red-500 text-sm no-underline">Esqueceu a senha?</a>
-                    </div>
-                    <Botao variante='adicionar' texto='Entrar' type='submit'></Botao>
-                    <MensagemCarregando />
-                    <MensagemErro texto={params.get("erro")} />
-                </form>
-            </div>
-        </>
+        <main className='flex flex-col items-center gap-y-8 flex-grow h-full'>
+            <Image src={sisrefLogo} alt="Sisref" />
+            <Form.Root className='flex flex-col gap-y-4 w-full min-w-[256px] max-w-[400px]' action={fetchLoginAPI}>
+                <Form.Field name='email' className='flex flex-col gap-y-2'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type='email' placeholder='estudante@ifce.edu.br' className='rounded py-2 px-4 outline outline-1 outline-cinza-600' />
+                </Form.Field>
+                <Form.Field name='password' className='flex flex-col gap-y-2'>
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type='password' placeholder='********' className='rounded py-2 px-4 outline outline-1 outline-cinza-600' />
+                </Form.Field>
+                <Form.Field name='remember' className='flex items-center gap-2'>
+                    <Form.Control type='checkbox' className='rounded border-2 border-cinza-600' />
+                    <Form.Label>Lembre-se de mim</Form.Label>
+                </Form.Field>
+                <MensagemErro texto={params.get("erro")} />
+                <MensagemCarregando />
+                <Form.Submit asChild>
+                    <Botao variante='adicionar' texto='Entrar' type='submit' />
+                </Form.Submit>
+            </Form.Root>
+       </main>
     );
 }
