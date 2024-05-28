3f915b6e783d8449cd3aa8213c58698e74cf4e3d
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-05-28 10:21:50 -0300

    feat(slider): adicionei efeito de hover as setas

--- sisref-v2-dashboard-client/src/app/elementos/componentes/Slider.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/Slider.tsx
@@ -14,12 +14,12 @@ interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {
 export const Slider = ({ texto, onPrevious, onNext, tooltip, ...rest }: SliderProps) => {
     return (
         <div className={`${rest.className} p-4 bg-cinza-600 text-center text-branco-400 font-bold rounded justify-between flex items-center`}>
-            <button onClick={onPrevious}>
-                <IconeSeta fill="fill-branco-400" direcao="esquerda" />
+            <button onClick={onPrevious} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
+                <IconeSeta fill='fill-branco-400' direcao="esquerda"/>
             </button>
             <span className='flex justify-center gap-x-2'>{texto}{tooltip}</span>
-            <button onClick={onNext}>
-                <IconeSeta fill="fill-branco-400" direcao="direita" />
+            <button onClick={onNext} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
+                <IconeSeta fill='fill-branco-400' direcao="direita"/>
             </button>
         </div>
     );
