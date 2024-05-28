a6e6ad5307e2ec35193594b1cfaaafcfa9dc8b9c
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-05-27 21:33:57 -0300

    feat(botao): adicionei um fade-in de ouline durante hover

--- sisref-v2-dashboard-client/src/app/elementos/basicos/Botao.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/basicos/Botao.tsx
@@ -8,15 +8,15 @@ interface BotaoProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const classNamePorVariante = {
-    adicionar: "bg-verde-300",
-    remover: "bg-branco-400 border-vermelho-400 border-[1px] text-vermelho-400",
-    editar: "bg-cinza-600 text-branco-400",
-    ocultar: "bg-branco-400 text-preto-400",
+    adicionar: "bg-verde-300 hover:outline-verde-400",
+    remover: "bg-branco-400 border-vermelho-400 border-[1px] text-vermelho-400 hover:outline-vermelho-400",
+    editar: "bg-cinza-600 text-branco-400 hover:outline-preto-400",
+    ocultar: "bg-branco-400 text-preto-400 hover:outline-preto-400",
 };
 
 export function Botao({ texto, variante, ...rest }: BotaoProps) {
     return (
-        <button className={`p-4 rounded-md text-branco-400 font-bold w-full ${classNamePorVariante[variante]} ${rest.className}`} {...rest}>
+        <button className={`p-4 rounded-md text-branco-400 font-bold w-full outline-2 hover:outline hover:animate-outline ${classNamePorVariante[variante]} ${rest.className}`} {...rest}>
             {texto}
         </button>
     );
