37d311681ee152aa545100e57b18cf75575cea8a
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-05-28 09:24:37 -0300

    feat(restricaoAlimentar): adicionei efeito ao hover

--- sisref-v2-dashboard-client/src/app/elementos/basicos/RestricaoAlimentar.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/basicos/RestricaoAlimentar.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React from 'react'
+import React, { EventHandler } from 'react'
 
 import { MouseEventHandler } from "react";
 import { IconeRestricaoAlimentar } from "./icones/IconeRestricaoAlimentar";
 
 interface RestricaoAlimentarProps {
     texto: string
-    onRemove?: MouseEventHandler<HTMLAnchorElement>
+    onRemove?: MouseEventHandler<HTMLButtonElement>
 }
 
 
@@ -18,8 +18,8 @@ export const RestricaoAlimentar = ({ texto, onRemove }: RestricaoAlimentarProps)
                 <IconeRestricaoAlimentar />
                 <p className="font-bold leading-4 text-sm text-preto-400">{texto}</p>
             </div>
-            <div className="flex gap-2 items-center relative text-vermelho-400">
-                <a onClick={onRemove} className="cursor-pointer">Remover</a>
+            <div className="flex gap-2 items-center relative text-vermelho-400 hover:font-medium">
+                <button onClick={onRemove} className="cursor-pointer">Remover</button>
             </div>
         </div>
 
