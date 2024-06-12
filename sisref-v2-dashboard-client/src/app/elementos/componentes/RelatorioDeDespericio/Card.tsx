60c9529d5b365af21cc1b3a2c903c6617dd19b13
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-06-12 09:31:56 -0300

    refactor(relatorioDeDesperdicio): adicionei o evento de click para abrir o modal a partir do card

--- sisref-v2-dashboard-client/src/app/elementos/componentes/RelatorioDeDespericio/Card.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/RelatorioDeDespericio/Card.tsx
@@ -1,20 +1,35 @@
 "use client"
 
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { DatasHelper } from "@/app/lib/elementos/DatasHelper";
 import { Botao } from "../../basicos/Botao";
 import { Secao } from "../../basicos/Secao";
+import { Modal } from "./Modal";
 
 export default function Card({ children, data }: { children: React.ReactNode, data: string }) {
+    useLayoutEffect(() => {
+        const modal = document.querySelector(`#modal-${data}`) as HTMLDialogElement;
+        modal.close();
+    }, [])
+
+    const handleClick = () => {
+        const modal = document.querySelector(`#modal-${data}`) as HTMLDialogElement;
+        modal.showModal();
+    }
+
     return (
-        <Secao className="flex flex-col gap-y-2 bg-azul-400">
-            <p className="font-bold">{DatasHelper.converterParaFormatoBrasileiro(data)}</p>
-
-            <div onClick={() => console.log(data)}>
+        <>
+            <Modal titulo="Relatório de desperdício" id={`modal-${data}`}>
                 {children}
-            </div>
+            </Modal>
 
-            <Botao variante="remover" texto="Remover"></Botao>
-        </Secao>
+            <Secao className="flex flex-col gap-y-2 bg-azul-400 cursor-pointer" onClick={handleClick}>
+                <p className="font-bold">{DatasHelper.converterParaFormatoBrasileiro(data)}</p>
+
+                {children}
+
+                <Botao variante="remover" texto="Remover"></Botao>
+            </Secao>
+        </>
     )
 }
--- sisref-v2-dashboard-client/src/app/elementos/componentes/RelatorioDeDespericio/Modal.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/RelatorioDeDespericio/Modal.tsx
@@ -18,13 +18,13 @@ const useModal = () => {
   return { modalRef, handleClose };
 }
 
-export const Modal = ({ children, titulo }: { children: React.ReactNode, titulo: string }) => {
+export const Modal = ({ children, titulo, id }: { children: React.ReactNode, titulo: string, id?: string }) => {
   const { modalRef, handleClose } = useModal();
 
   const handleKeyDown = (event: React.KeyboardEvent) => event.key === "Enter" && handleClose();
 
   return (
-    <dialog ref={modalRef} className="fixed inset-8 z-10 max-h-[calc(100svh-8rem)] max-w-[768px] bottom-8 my-8 px-6 py-4 rounded backdrop:bg-preto-400/15 backdrop:backdrop-blur-sm overflow-y-auto">
+    <dialog id={id || ""} ref={modalRef} className="fixed inset-8 z-10 max-h-[calc(100svh-8rem)] max-w-[768px] bottom-8 my-8 px-6 py-4 rounded backdrop:bg-preto-400/15 backdrop:backdrop-blur-sm overflow-y-auto">
       <div className="flex justify-between items-center mb-4 gap-x-4">
         <h2 className="text-xl font-bold ">{titulo}</h2>
         <IconeFechar onClick={handleClose} onKeyDown={handleKeyDown} tabIndex={1} className="fill-cinza-600"/>
