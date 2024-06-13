1428c5808a6dc6fcb132441399f0b7a08618512a
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-06-13 10:33:51 -0300

    refactor(SliderServerSide): adicionei 'replace' para limpar o historico de navegacao

--- sisref-v2-dashboard-client/src/app/elementos/componentes/SliderServerSide.tsx
+++ sisref-v2-dashboard-client/src/app/elementos/componentes/SliderServerSide.tsx
@@ -15,11 +15,11 @@ interface SliderServerSideProps extends React.HTMLAttributes<HTMLDivElement> {
 export const SliderServerSide = ({ texto, onPrevious, onNext, tooltip, ...rest }: SliderServerSideProps) => {
     return (
         <div className={`${rest.className} p-4 bg-cinza-600 text-center text-branco-400 font-bold rounded justify-between flex items-center`}>
-            <Link href={onPrevious} scroll={false} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
+            <Link href={onPrevious} replace scroll={false} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
                 <IconeSeta fill='fill-branco-400' direcao="esquerda"/>
             </Link>
             <span className='flex justify-center gap-x-2'>{texto}{tooltip}</span>
-            <Link href={onNext}  scroll={false} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
+            <Link href={onNext} replace  scroll={false} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
                 <IconeSeta fill='fill-branco-400' direcao="direita"/>
             </Link>
         </div>
