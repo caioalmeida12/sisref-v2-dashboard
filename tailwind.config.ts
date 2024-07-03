0c79ded95ea7e8fe431251f8bf55844ab13856ac
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-03 07:43:04 -0300

    fix(react-datepicker): corrigi erro de estilizacao do componente

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "../node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     extend: {
