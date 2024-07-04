ef2b4b73d49a6b4a6cc4128dc24333dfdd428e59
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-04 08:58:58 -0300

    chore(plate): adicionei mais opcoes ao insert-dropdown-menu

--- src/app/elementos/componentes/plate-ui/insert-dropdown-menu.tsx
+++ src/app/elementos/componentes/plate-ui/insert-dropdown-menu.tsx
@@ -6,41 +6,15 @@ import type { DropdownMenuProps } from '@radix-ui/react-dropdown-menu';
 
 import { ELEMENT_BLOCKQUOTE } from '@udecode/plate-block-quote';
 import {
-  ELEMENT_CODE_BLOCK,
-  insertEmptyCodeBlock,
-} from '@udecode/plate-code-block';
-import {
   focusEditor,
   insertEmptyElement,
   useEditorRef,
 } from '@udecode/plate-common';
-import { ELEMENT_EXCALIDRAW } from '@udecode/plate-excalidraw';
-import {
-  ELEMENT_H1,
-  ELEMENT_H2,
-  ELEMENT_H3,
-  ELEMENT_H4,
-  ELEMENT_H5,
-  ELEMENT_H6,
-} from '@udecode/plate-heading';
-import { ELEMENT_HR } from '@udecode/plate-horizontal-rule';
-import {
-  KEY_LIST_STYLE_TYPE,
-  toggleIndentList,
-} from '@udecode/plate-indent-list';
-import { ELEMENT_COLUMN_GROUP, insertColumnGroup } from '@udecode/plate-layout';
-import { ELEMENT_LINK, triggerFloatingLink } from '@udecode/plate-link';
-import { toggleList } from '@udecode/plate-list';
-import {
-  ELEMENT_IMAGE,
-  ELEMENT_MEDIA_EMBED,
-  insertMedia,
-} from '@udecode/plate-media';
+import { ELEMENT_H1, ELEMENT_H2, ELEMENT_H3 } from '@udecode/plate-heading';
 import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
-import { ELEMENT_TABLE, insertTable } from '@udecode/plate-table';
 
-import { settingsStore } from '@/components/context/settings-store';
 import { Icons } from '@/components/icons';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -49,143 +23,124 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
   useOpenState,
-} from '@/registry/default/plate-ui/dropdown-menu';
-import { ToolbarButton } from '@/registry/default/plate-ui/toolbar';
+} from './dropdown-menu';
+import { ToolbarButton } from './toolbar';
+import { ELEMENT_TABLE } from '@udecode/plate-table';
+import { ELEMENT_HR } from '@udecode/plate-horizontal-rule';
+import { ELEMENT_CODE_BLOCK } from '@udecode/plate-code-block';
+import { ELEMENT_EXCALIDRAW } from '@udecode/plate-excalidraw';
+import { ELEMENT_LINK } from '@udecode/plate-link';
+import { ELEMENT_IMAGE } from '@udecode/plate-media';
 
 const items = [
   {
     items: [
       {
-        description: 'Paragraph',
+        description: 'Parágrafo',
         icon: Icons.paragraph,
-        label: 'Paragraph',
+        label: 'Parágrafo',
         value: ELEMENT_PARAGRAPH,
       },
       {
-        description: 'Heading 1',
+        description: 'Título 1',
         icon: Icons.h1,
-        label: 'Heading 1',
+        label: 'Título 1',
         value: ELEMENT_H1,
       },
       {
-        description: 'Heading 2',
+        description: 'Título 2',
         icon: Icons.h2,
-        label: 'Heading 2',
+        label: 'Título 2',
         value: ELEMENT_H2,
       },
       {
-        description: 'Heading 3',
+        description: 'Título 3',
         icon: Icons.h3,
-        label: 'Heading 3',
+        label: 'Título 3',
         value: ELEMENT_H3,
       },
       {
-        description: 'Heading 4',
-        icon: Icons.h4,
-        label: 'Heading 4',
-        value: ELEMENT_H4,
-      },
-      {
-        description: 'Heading 5',
-        icon: Icons.h5,
-        label: 'Heading 5',
-        value: ELEMENT_H5,
-      },
-      {
-        description: 'Heading 6',
-        icon: Icons.h6,
-        label: 'Heading 6',
-        value: ELEMENT_H6,
-      },
-      {
-        description: 'Table',
-        icon: Icons.table,
-        label: 'Table',
-        value: ELEMENT_TABLE,
-      },
-      {
-        description: 'Bulleted list',
-        icon: Icons.ul,
-        label: 'Bulleted list',
-        value: 'ul',
-      },
-      {
-        description: 'Numbered list',
-        icon: Icons.ol,
-        label: 'Numbered list',
-        value: 'ol',
-      },
-      {
-        description: 'Quote (⌘+⇧+.)',
+        description: 'Citação (Ctrl+Shift+.)',
         icon: Icons.blockquote,
-        label: 'Quote',
+        label: 'Citação',
         value: ELEMENT_BLOCKQUOTE,
       },
       {
-        description: 'Divider (---)',
-        icon: Icons.hr,
-        label: 'Divider',
-        value: ELEMENT_HR,
+        value: ELEMENT_TABLE,
+        label: 'Tabela',
+        description: 'Tabela',
+        icon: Icons.table,
       },
       {
-        description: 'Columns',
-        icon: Icons.LayoutIcon,
-        label: 'Columns',
-        value: ELEMENT_COLUMN_GROUP,
+        value: 'ul',
+        label: 'Lista com Marcadores',
+        description: 'Lista com Marcadores',
+        icon: Icons.ul,
+      },
+      {
+        value: 'ol',
+        label: 'Lista Numerada',
+        description: 'Lista Numerada',
+        icon: Icons.ol,
+      },
+      {
+        value: ELEMENT_HR,
+        label: 'Divisor',
+        description: 'Divisor (---)',
+        icon: Icons.hr,
       },
     ],
-    label: 'Basic blocks',
+    label: 'Blocos Básicos',
   },
   {
+    label: 'Mídia',
     items: [
       {
-        description: 'Code (```)',
-        icon: Icons.codeblock,
-        label: 'Code',
         value: ELEMENT_CODE_BLOCK,
+        label: 'Código',
+        description: 'Código (```)',
+        icon: Icons.codeblock,
       },
       {
-        description: 'Image',
-        icon: Icons.image,
-        label: 'Image',
         value: ELEMENT_IMAGE,
+        label: 'Imagem',
+        description: 'Imagem',
+        icon: Icons.image,
       },
+      // {
+      //   value: ELEMENT_MEDIA_EMBED,
+      //   label: 'Incorporar',
+      //   description: 'Incorporar',
+      //   icon: Icons.embed,
+      // },
       {
-        description: 'Embed',
-        icon: Icons.embed,
-        label: 'Embed',
-        value: ELEMENT_MEDIA_EMBED,
-      },
-      {
+        value: ELEMENT_EXCALIDRAW,
+        label: 'Excalidraw',
         description: 'Excalidraw',
         icon: Icons.excalidraw,
-        label: 'Excalidraw',
-        value: ELEMENT_EXCALIDRAW,
       },
     ],
-    label: 'Media',
   },
   {
+    label: 'Em linha',
     items: [
       {
+        value: ELEMENT_LINK,
+        label: 'Link',
         description: 'Link',
         icon: Icons.link,
-        label: 'Link',
-        value: ELEMENT_LINK,
       },
     ],
-    label: 'Inline',
   },
 ];
-
-export function PlaygroundInsertDropdownMenu(props: DropdownMenuProps) {
+export function InsertDropdownMenu(props: DropdownMenuProps) {
   const editor = useEditorRef();
   const openState = useOpenState();
 
   return (
     <DropdownMenu modal={false} {...openState} {...props}>
       <DropdownMenuTrigger asChild>
-        <ToolbarButton isDropdown pressed={openState.open} tooltip="Insert">
+        <ToolbarButton isDropdown pressed={openState.open} tooltip="Inserir">
           <Icons.add />
         </ToolbarButton>
       </DropdownMenuTrigger>
@@ -204,57 +159,52 @@ export function PlaygroundInsertDropdownMenu(props: DropdownMenuProps) {
                 <DropdownMenuItem
                   className="min-w-[180px]"
                   key={type}
-                  onSelect={async () => {
+                  onSelect={() => {
                     switch (type) {
-                      case ELEMENT_COLUMN_GROUP: {
-                        insertColumnGroup(editor);
-
-                        break;
-                      }
-                      case ELEMENT_CODE_BLOCK: {
-                        insertEmptyCodeBlock(editor);
-
-                        break;
-                      }
-                      case ELEMENT_IMAGE: {
-                        await insertMedia(editor, { type: ELEMENT_IMAGE });
-
-                        break;
-                      }
-                      case ELEMENT_MEDIA_EMBED: {
-                        await insertMedia(editor, {
-                          type: ELEMENT_MEDIA_EMBED,
-                        });
-
-                        break;
-                      }
-                      case 'ul':
-                      case 'ol': {
-                        insertEmptyElement(editor, ELEMENT_PARAGRAPH, {
-                          nextBlock: true,
-                          select: true,
-                        });
-
-                        if (settingsStore.get.checkedId(KEY_LIST_STYLE_TYPE)) {
-                          toggleIndentList(editor, {
-                            listStyleType: type === 'ul' ? 'disc' : 'decimal',
-                          });
-                        } else if (settingsStore.get.checkedId('list')) {
-                          toggleList(editor, { type });
-                        }
-
-                        break;
-                      }
-                      case ELEMENT_TABLE: {
-                        insertTable(editor);
-
-                        break;
-                      }
-                      case ELEMENT_LINK: {
-                        triggerFloatingLink(editor, { focused: true });
-
-                        break;
-                      }
+                      // case ELEMENT_CODE_BLOCK: {
+                      //   insertEmptyCodeBlock(editor);
+                      //
+                      //   break;
+                      // }
+                      // case ELEMENT_IMAGE: {
+                      //   await insertMedia(editor, { type: ELEMENT_IMAGE });
+                      //
+                      //   break;
+                      // }
+                      // case ELEMENT_MEDIA_EMBED: {
+                      //   await insertMedia(editor, {
+                      //     type: ELEMENT_MEDIA_EMBED,
+                      //   });
+                      //
+                      //   break;
+                      // }
+                      // case 'ul':
+                      // case 'ol': {
+                      //   insertEmptyElement(editor, ELEMENT_PARAGRAPH, {
+                      //     select: true,
+                      //     nextBlock: true,
+                      //   });
+                      //
+                      //   if (settingsStore.get.checkedId(KEY_LIST_STYLE_TYPE)) {
+                      //     toggleIndentList(editor, {
+                      //       listStyleType: type === 'ul' ? 'disc' : 'decimal',
+                      //     });
+                      //   } else if (settingsStore.get.checkedId('list')) {
+                      //     toggleList(editor, { type });
+                      //   }
+                      //
+                      //   break;
+                      // }
+                      // case ELEMENT_TABLE: {
+                      //   insertTable(editor);
+                      //
+                      //   break;
+                      // }
+                      // case ELEMENT_LINK: {
+                      //   triggerFloatingLink(editor, { focused: true });
+                      //
+                      //   break;
+                      // }
                       default: {
                         insertEmptyElement(editor, type, {
                           nextBlock: true,
