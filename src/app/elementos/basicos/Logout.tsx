5bd720e7b2b66251a50fb19db543a968b1035a7a
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-08 10:57:45 -0300

    chore(accessibility): adicionei a propriedade name aos botoes

--- src/app/elementos/basicos/Logout.tsx
+++ src/app/elementos/basicos/Logout.tsx
@@ -7,7 +7,7 @@ import React from "react"
 export const Logout = () => {
     const handleLogout = async () => await logout()
 
-    return <button onClick={handleLogout} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
+    return <button name="Sair" onClick={handleLogout} className='relative before:content-[""] before:inset-[-.5em] before:rounded before:opacity-10 before:bg-branco-400 hover:before:absolute'>
         <IconeLogout />
     </button>
 }
--- src/app/elementos/basicos/RestricaoAlimentar.tsx
+++ src/app/elementos/basicos/RestricaoAlimentar.tsx
@@ -18,7 +18,7 @@ export const RestricaoAlimentar = ({ texto, onRemove }: RestricaoAlimentarProps)
                 <p className="font-bold leading-4 text-sm text-preto-400">{texto}</p>
             </div>
             <div className="flex gap-2 items-center relative text-vermelho-400 hover:font-medium">
-                <button onClick={onRemove} className="cursor-pointer">Remover</button>
+                <button name='Remover' onClick={onRemove} className="cursor-pointer">Remover</button>
             </div>
         </div>
 
