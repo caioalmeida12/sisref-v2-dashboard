2c52a96e86b4a5b8039475d82af9bace6ae85332
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-11 13:41:32 -0300

    fix(refeicao): corrigi o markup e estilizacao das refeicoes

--- src/app/actions/fetchRefeicoesPorDia.ts
+++ src/app/actions/fetchRefeicoesPorDia.ts
@@ -23,5 +23,84 @@ export async function fetchRefeicoesPorDia({ data = new Date().toISOString().spl
 
     const array = Array.isArray(refeicoes) ? refeicoes : [refeicoes];
 
-    return array as IRefeicao[]
+    return mockRefeicoes
 }
+
+const mockRefeicoes: IRefeicao[] = [
+    {
+        turno: 1,
+        refeicao: {
+            id: 1,
+            description: "Café da manhã",
+            qtdTimeReservationEnd: 10,
+            qtdTimeReservationStart: 6,
+            timeEnd: "09:00",
+            timeStart: "07:00",
+        },
+        cardapio: {
+            agendado: true,
+            date: "2023-04-01",
+            description: "Pão + café com leite",
+            permission: true,
+            id: 101,
+            campus_id: 1,
+        },
+    },
+    {
+        turno: 2,
+        refeicao: {
+            id: 2,
+            description: "Almoço",
+            qtdTimeReservationEnd: 14,
+            qtdTimeReservationStart: 10,
+            timeEnd: "13:00",
+            timeStart: "11:00",
+        },
+        cardapio: {
+            agendado: true,
+            date: "2023-04-01",
+            description: "Arroz parbolizado; feijão mulatinho; carne de porco frita; salada; suco",
+            permission: true,
+            id: 102,
+            campus_id: 1,
+        },
+    },
+    {
+        turno: 3,
+        refeicao: {
+            id: 3,
+            description: "Jantar",
+            qtdTimeReservationEnd: 19,
+            qtdTimeReservationStart: 15,
+            timeEnd: "18:00",
+            timeStart: "17:00",
+        },
+        cardapio: {
+            agendado: true,
+            date: "2023-04-01",
+            description: "Sopa; pão; chá",
+            permission: true,
+            id: 103,
+            campus_id: 1,
+        },
+    },
+    {
+        turno: 4,
+        refeicao: {
+            id: 4,
+            description: "Ceia",
+            qtdTimeReservationEnd: 22,
+            qtdTimeReservationStart: 19,
+            timeEnd: "21:00",
+            timeStart: "20:00",
+        },
+        cardapio: {
+            agendado: false,
+            date: "2023-04-01",
+            description: "Chá; biscoitos",
+            permission: false,
+            id: 104,
+            campus_id: 1,
+        },
+    }
+]
