d7e0883f28cb4ffaa5854684413b99c0a0b69aa9
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-07-24 10:14:45 -0300

    refactor(page, Footer): Corrigido o posicionamento do footer e a marca do fundo cinza

--- src/app/elementos/componentes/Footer.tsx
+++ src/app/elementos/componentes/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer: React.FC = () => {
     return (
-        <footer className="bg-verde-400 text-center px-4 py-3 md:py-5">
+        <footer className="bg-verde-400 text-center px-4 py-3 md:py-5 mt-auto">
             <p className="text-sm text-branco-400 md:text-base">Instituto Federal do Ceará - Campus Cedro</p>
         </footer>
     );
