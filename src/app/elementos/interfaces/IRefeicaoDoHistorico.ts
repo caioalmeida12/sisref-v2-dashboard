71ec3f648587057b715b6cd66b82e2f14e53bfa2
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-05 16:45:10 -0300

    feat(zod): integrei a interface ao Zod

--- src/app/elementos/interfaces/IRefeicaoDoHistorico.ts
+++ src/app/elementos/interfaces/IRefeicaoDoHistorico.ts
@@ -1,22 +1,40 @@
-export interface IRefeicaoDoHistorico {
-    ticket_id: number;
-    turno: 1 | 2 | 3 | 4;
-    cardapio: {
-        id: number;
-        date: string;
-        description: string;
-        campus_id: number;
-        meal_id: number;
-    };
-    refeicao: {
-        id: number;
-        description: string;
-        timeEnd: string;
-        timeStart: string;
-        campus_id: number;
-        qtdTimeReservationEnd: number;
-        qtdTimeReservationStart: number;
-    };
-    status: "a-ser-utilizado" | "utilizado" | "cancelado" | "nao-utilizado" | "justificado" | "nao-utilizado-sem-justificativa";
-    absenceJustification: string | null
+import z from 'zod';
+
+const IRefeicaoDoHistoricoSchema = z.object({
+    id: z.number(),
+    ticket_id: z.number().optional(),
+    turno: z.preprocess((num) => String(num), z.enum(["1", "2", "3", "4"]).optional()),
+    cardapio: z.object({
+        id: z.number(),
+        date: z.string(),
+        description: z.string(),
+        campus_id: z.number(),
+        meal_id: z.number(),
+    }),
+    refeicao: z.object({
+        id: z.number(),
+        description: z.string(),
+        timeEnd: z.string(),
+        timeStart: z.string(),
+        campus_id: z.number(),
+        qtdTimeReservationEnd: z.number(),
+        qtdTimeReservationStart: z.number(),
+    }),
+    status: z.string().optional(),
+    absenceJustification: z.string().nullable(),
+    studentJustification: z.string().nullable(),
+});
+
+export const ParseRefeicaoDoHistorico = (refeicao: any) => {
+    return typeof refeicao === "object" ? IRefeicaoDoHistoricoSchema.safeParse({
+        ...refeicao,
+        ticket_id: refeicao.id,
+        turno: refeicao.meal_id,
+        cardapio: refeicao.menu,
+        refeicao: refeicao.meal,
+    }) : { success: false, data: null }
 }
+
+export const IRefeicaoDoHistorico = IRefeicaoDoHistoricoSchema;
+
+export type IRefeicaoDoHistorico = z.infer<typeof IRefeicaoDoHistoricoSchema>;
