d4b7bed2ce7e0679ebe3710a9d276c2cde51281f
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-09 15:57:23 -0300

    fix(middleware): corrigi a captura de erros de redirecionamento do Next

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
-import { NextRequest} from 'next/server'
+import { NextRequest } from 'next/server'
 import { requerAutorizacaoMiddleware } from './app/lib/middlewares/RequerAutorizacaoMiddleware';
+import { isRedirectError } from 'next/dist/client/components/redirect';
 
 export default async function middleware(requisicao: NextRequest) {
-  const resposta = await requerAutorizacaoMiddleware(requisicao)
+  const resposta = await requerAutorizacaoMiddleware(requisicao).catch((erro) => {
+    // Quando a função redirecionarViaMiddleware é chamada, um erro de redirecionamento é lançado. 
+    // O next.js trata esse erro e redireciona o usuário para a página de login automaticamente.
+    // Se o erro for um erro de redirecionamento, não queremos que ele seja tratado como outro tipo de erro. 
+    // Assim, retornamos o erro e deixamos o next lidar com isso.
+    if (isRedirectError(erro)) return erro
+
+    throw erro
+  })
 
   return resposta
 }
