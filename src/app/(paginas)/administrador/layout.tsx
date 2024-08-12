9cc2801eb9913ff0f4cba1802808afc24bfe346b
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-12 16:27:43 -0300

    feat(paginas): criei as paginas dos demais tipos de usuario

---
+++ src/app/(paginas)/administrador/layout.tsx
@@ -0,0 +1,8 @@
+export default function AdministradorlLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <h1>Administrador layout</h1>
+            {children}
+        </div>
+    );
+}
---
+++ src/app/(paginas)/assistencia_estudantil/layout.tsx
@@ -0,0 +1,8 @@
+export default function AssistenciaEstudantilLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <h1>Assistencia estudantil layout</h1>
+            {children}
+        </div>
+    );
+}
