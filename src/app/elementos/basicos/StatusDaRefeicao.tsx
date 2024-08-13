23329f8e3c3e29f16327563553699726a1b976e4
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-13 12:09:48 -0300

    fix(build): corrigi um erro de importacao de interface

--- src/app/elementos/basicos/StatusDaRefeicao.tsx
+++ src/app/elementos/basicos/StatusDaRefeicao.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Tooltip from '@radix-ui/react-tooltip';
 
-import { IIconeStatusProps } from "@elementos/interfaces/IIconeStatusProps";
 import Icone from './icones';
+import { IIconeStatusProps } from '../interfaces/IIcones';
 
 interface StatusDaRefeicaoProps {
     texto: string;
