c8b563b0f53ccbc80b8c14992d46f61615aaa5f5
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-14 08:34:33 -0300

    feat(aviso): integrei o aviso com a api de tickets sem justificativa

--- src/app/(paginas)/(estudante)/page.tsx
+++ src/app/(paginas)/(estudante)/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Aviso } from '@/app/elementos/modulos/Aviso';
 import { InformacoesDeEstudante } from '@/app/elementos/modulos/InformacoesDeEstudante';
+import { fetchTicketsSemJustificativa } from '@/app/actions/fetchTicketsSemJustificativa';
 
 export default async function Home() {
+
+  const temTicketsSemJustificativa = await fetchTicketsSemJustificativa()
+
   return (
     <>
       <InformacoesDeEstudante />
-      <Aviso titulo='Aviso' texto='Você possui uma refeição pendente. Por favor, vá até a cantina para retirá-la.' />
+      {
+        temTicketsSemJustificativa.length ?
+          <Aviso titulo='Aviso' texto='Você possui um ou mais tickets pendentes de justificativa. Por favor, acesse o histórico de refeições para regularizar sua situação.' />
+          : null
+      }
     </>
   );
 }
