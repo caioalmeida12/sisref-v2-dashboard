4b07f25eb0d550422e1c60c1ff71097189486c88
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-19 09:23:23 -0300

    fix(tooltips): corrigi inconsistencias de estilizacao

--- src/app/elementos/basicos/StatusDaRefeicao.tsx
+++ src/app/elementos/basicos/StatusDaRefeicao.tsx
@@ -52,7 +52,7 @@ export const StatusDaRefeicao = ({ texto, cor, icone, textoTooltip }: StatusDaRe
                 {textoTooltip && (
                     <Tooltip.Portal>
                         <Tooltip.Content
-                            className="bg-branco-400 border border-cinza-600 shadow-lg rounded-md p-2 text-sm max-w-[calc(100vw-2rem)]"
+                            className="bg-branco-400 border border-cinza-600 max-w-[calc(100vw-2rem)] data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] "
                             sideOffset={5}
                         >
                             {textoTooltip}
--- src/app/elementos/modulos/RefeicoesPorDia.tsx
+++ src/app/elementos/modulos/RefeicoesPorDia.tsx
@@ -65,7 +65,7 @@ export const RefeicoesPorDia = ({ forcarExibicao = false }: { forcarExibicao?: b
                                 </Tooltip.Trigger>
                                 <Tooltip.Portal>
                                     <Tooltip.Content
-                                        className="bg-branco-400 border data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+                                        className="bg-branco-400 border border-cinza-600 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
                                         sideOffset={5}
                                     >
                                         <div>
