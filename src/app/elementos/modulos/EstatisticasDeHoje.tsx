207244b03b2e6516f9875914619fd17ca9485fdd
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-19 10:41:34 -0300

    fix(build): corrigi erros de building

--- src/app/elementos/modulos/EstatisticasDeHoje.tsx
+++ src/app/elementos/modulos/EstatisticasDeHoje.tsx
@@ -12,6 +12,7 @@ import { TicketsPorDia } from "../componentes/TicketsPorDia";
 import { TicketsPorRefeicao } from "../componentes/TicketsPorRefeicao";
 import { useQuery } from "@tanstack/react-query";
 import Icone from "../basicos/Icone";
+import { CustomTooltipWrapper } from "../basicos/CustomTooltipWrapper";
 
 export default function EstatisticasDeHoje() {
     const [dataDaPesquisa, setDataDaPesquisa] = useState(new Date().toISOString().split('T')[0]);
@@ -36,9 +37,20 @@ export default function EstatisticasDeHoje() {
                 }}
 
                 tooltip={
-                    (refeicoes?.length) ? null : (
-                        <Icone.Informacao texto="Nenhuma refeição encontrada para esta data" />
-                    )
+                    <CustomTooltipWrapper
+                        elementoContent={
+                            <p>
+                                Nenhuma estatística encontrada para esta data.
+                            </p>
+                        }
+                        elementoTrigger={
+                            <div className="my-auto">
+                                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="fill-vermelho-400">
+                                    <path d="M6.6 4.2H5.4V3H6.6M6.6 9H5.4V5.4H6.6M6 0C5.21207 0 4.43185 0.155195 3.7039 0.456723C2.97595 0.758251 2.31451 1.20021 1.75736 1.75736C0.632141 2.88258 0 4.4087 0 6C0 7.5913 0.632141 9.11742 1.75736 10.2426C2.31451 10.7998 2.97595 11.2417 3.7039 11.5433C4.43185 11.8448 5.21207 12 6 12C7.5913 12 9.11742 11.3679 10.2426 10.2426C11.3679 9.11742 12 7.5913 12 6C12 5.21207 11.8448 4.43185 11.5433 3.7039C11.2417 2.97595 10.7998 2.31451 10.2426 1.75736C9.68549 1.20021 9.02405 0.758251 8.2961 0.456723C7.56815 0.155195 6.78793 0 6 0Z" />
+                                </svg>
+                            </div>
+                        }
+                    />
                 }
             />
             {
