030bb3ebc996877082b371b92ce2772ad4b58c51
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-22 18:35:31 -0300

    temp(tabela): implementando a tabela

--- src/app/elementos/basicos/Badge.tsx
+++ src/app/elementos/basicos/Badge.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import classnames from 'classnames';
 
 interface BadgeProps {
     texto: string;
     corDaBadge: string;
+    className?: string;
 }
 
-export const Badge: React.FC<BadgeProps> = ({ texto, corDaBadge }) => (
-    <span className={`text-branco-400 px-4 font-bold rounded ${corDaBadge} border-[1px]`}>{texto}</span>
+export const Badge: React.FC<BadgeProps> = ({ texto, corDaBadge, className }) => (
+    <span className={classnames('text-branco-400', 'px-4', 'font-bold', 'rounded', corDaBadge, 'border-[1px]', className)}>
+        {texto}
+    </span>
 );
