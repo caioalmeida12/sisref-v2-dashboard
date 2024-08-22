1b27a4a78d92bfc192c13e9ee10a062737bf84b9
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-22 18:04:35 -0300

    feat(api): implementei a busca das refeicoes e cardapios por dia

--- src/app/(paginas)/(administrativas)/nutricionista/page.tsx
+++ src/app/(paginas)/(administrativas)/nutricionista/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { Secao } from "@/app/elementos/basicos/Secao";
 import { CabecalhoDeSecao } from "@/app/elementos/basicos/CabecalhoDeSecao";
@@ -9,22 +9,13 @@ import * as Form from '@radix-ui/react-form';
 import * as Select from '@radix-ui/react-select';
 import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
 import { useQuery } from "@tanstack/react-query";
-import { fetchNomesDeRefeicoesNutricionista } from "@/app/actions/fetchRefeicoesNutricionista";
-import DataTable from 'react-data-table-component';
+import { fetchTabelaDeRefeicoesNutricionista } from "@/app/actions/fetchTabelaDeRefeicoesRefeicoes";
+import { fetchNomesDeRefeicoesNutricionista } from "@/app/actions/fetchNomesDeRefeicoesNutricionista";
+
 
 interface NutricionistaPageProps {
   params: { slug: string }
   searchParams: { [key: string]: string | string[] | undefined }
-}
-
-const searchParamsToURLSearchParams = (searchParams: { [key: string]: string | string[] | undefined }): URLSearchParams => {
-  const urlSearchParams = new URLSearchParams()
-  for (const key in searchParams) {
-    if (searchParams[key] !== undefined) {
-      urlSearchParams.set(key, searchParams[key] as string)
-    }
-  }
-  return urlSearchParams
 }
 
 export default function NutricionistaPage({
@@ -32,25 +23,39 @@ export default function NutricionistaPage({
   params,
   searchParams
 }: NutricionistaPageProps) {
-
-  // Sempre passar um novo objeto para evitar mutações
-  const passarSearchParams = () => new URLSearchParams(searchParamsToURLSearchParams(searchParams).toString())
-
   // As datas são armazenadas no formato  yyyy-MM-dd
   const [datas, setDatas] = useState<{ dataInicial: string, dataFinal: string }>({
     dataInicial: new Date().toISOString().split('T')[0],
     dataFinal: new Date().toISOString().split('T')[0]
-  })
+  });
+
+  const dataInicialRef = useRef<HTMLInputElement>(null);
+  const dataFinalRef = useRef<HTMLInputElement>(null);
+  const nomeDeRefeicaoRef = useRef<HTMLFormElement>(null);
 
   const { data: refeicoes, isLoading: isLoadingRefeicoes } = useQuery({
     queryKey: ['refeicoes', datas],
-    queryFn: () => fetchNomesDeRefeicoesNutricionista(datas)
+    queryFn: () => fetchNomesDeRefeicoesNutricionista()
   });
 
-  const { data: tabela, isLoading: isLoadingTabela } = useQuery({
-    queryKey: ['tabela', datas],
-    queryFn: () => fetchTabelaDeRefeicoesNutricionista(datas)
+  const { data: dadosDaTabela, isLoading: isLoadingDadosDaTabela } = useQuery({
+    queryKey: ['tabela', datas, nomeDeRefeicaoRef.current?.querySelector('select')?.value],
+    queryFn: () => fetchTabelaDeRefeicoesNutricionista({ campus_id: 1, date: datas.dataInicial }),
   });
+
+
+  const handleBuscar = () => {
+    const dataInicial = dataInicialRef.current?.querySelector('input')?.value;
+    const dataFinal = dataFinalRef.current?.querySelector('input')?.value;
+    const nomeDeRefeicao = nomeDeRefeicaoRef.current?.querySelector('select')?.value;
+
+    console.log(dataInicial, dataFinal, nomeDeRefeicao);
+
+    setDatas({
+      dataInicial: dataInicial || datas.dataInicial,
+      dataFinal: dataFinal || datas.dataFinal
+    });
+  };
 
   return (
     <>
@@ -60,7 +65,7 @@ export default function NutricionistaPage({
           <Secao className="flex">
             <div className="flex gap-x-4 items-end">
               <Form.Root className="flex">
-                <Form.Field name="dataInicial" className="flex flex-col gap-y-2">
+                <Form.Field name="dataInicial" className="flex flex-col gap-y-2" ref={dataInicialRef}>
                   <Form.Label className="font-bold">
                     Data Inicial
                   </Form.Label>
@@ -69,7 +74,7 @@ export default function NutricionistaPage({
                 <Form.Submit />
               </Form.Root>
               <Form.Root className="flex">
-                <Form.Field name="dataInicial" className="flex flex-col gap-y-2">
+                <Form.Field name="dataInicial" className="flex flex-col gap-y-2" ref={dataFinalRef}>
                   <Form.Label className="font-bold">
                     Data Final
                   </Form.Label>
@@ -79,49 +84,62 @@ export default function NutricionistaPage({
               </Form.Root>
               <div className="flex flex-col gap-y-2">
                 <label className="font-bold">Refeição</label>
-                <Select.Root>
-                  <Select.Trigger className="px-2 py-1 h-[34px] flex overflow-hidden items-center min-w-[250px] text-left rounded outline outline-1 outline-cinza-600">
-                    <Select.Value placeholder="Todas as refeições" defaultValue={"0"} />
-                    <Select.Icon className="SelectIcon">
-                      <ChevronDownIcon />
-                    </Select.Icon>
-                  </Select.Trigger>
+                <form ref={nomeDeRefeicaoRef}>
+                  <Select.Root>
+                    <Select.Trigger className="px-2 py-1 h-[34px] flex overflow-hidden items-center min-w-[250px] text-left rounded outline outline-1 outline-cinza-600">
+                      <Select.Value placeholder="Todas as refeições" defaultValue={"0"} />
+                      <Select.Icon className="SelectIcon">
+                        <ChevronDownIcon />
+                      </Select.Icon>
+                    </Select.Trigger>
 
-                  <Select.Portal>
-                    <Select.Content>
-                      <Select.ScrollUpButton />
-                      <Select.Viewport className="bg-branco-400 px-2 py-1 rounded outline outline-1 outline-cinza-600">
-                        <Select.Item value={"0"} className="flex items-center px-2 py-1 hover:outline outline-1 rounded hover:bg-amarelo-200">
-                          <Select.ItemText>
-                            Todas as refeições
-                          </Select.ItemText>
-                          <Select.ItemIndicator>
-                            <CheckIcon />
-                          </Select.ItemIndicator>
-                        </Select.Item>
-                        {
-                          !isLoadingRefeicoes && refeicoes &&
-                          refeicoes.map((refeicao, index) => (
-                            <Select.Item value={String(refeicao?.id)} className="flex items-center px-2 py-1 hover:outline outline-1 rounded hover:bg-amarelo-200" key={index}>
-                              <Select.ItemText>
-                                {refeicao?.description}
-                              </Select.ItemText>
-                              <Select.ItemIndicator>
-                                <CheckIcon />
-                              </Select.ItemIndicator>
-                            </Select.Item>
-                          ))
-                        }
-
-                      </Select.Viewport>
-                      <Select.ScrollDownButton />
-                      <Select.Arrow />
-                    </Select.Content>
-                  </Select.Portal>
-                </Select.Root>
+                    <Select.Portal>
+                      <Select.Content>
+                        <Select.ScrollUpButton />
+                        <Select.Viewport className="bg-branco-400 px-2 py-1 rounded outline outline-1 outline-cinza-600">
+                          <Select.Item value={"0"} className="flex items-center px-2 py-1 hover:outline outline-1 rounded hover:bg-amarelo-200">
+                            <Select.ItemText>
+                              Todas as refeições
+                            </Select.ItemText>
+                            <Select.ItemIndicator>
+                              <CheckIcon />
+                            </Select.ItemIndicator>
+                          </Select.Item>
+                          {
+                            !isLoadingRefeicoes && refeicoes &&
+                            refeicoes.map((refeicao, index) => (
+                              <Select.Item value={String(refeicao?.id)} className="flex items-center px-2 py-1 hover:outline outline-1 rounded hover:bg-amarelo-200" key={index}>
+                                <Select.ItemText>
+                                  {refeicao?.description}
+                                </Select.ItemText>
+                                <Select.ItemIndicator>
+                                  <CheckIcon />
+                                </Select.ItemIndicator>
+                              </Select.Item>
+                            ))
+                          }
+                        </Select.Viewport>
+                        <Select.ScrollDownButton />
+                        <Select.Arrow />
+                      </Select.Content>
+                    </Select.Portal>
+                  </Select.Root>
+                </form>
               </div>
-              <Botao variante="adicionar" texto="Buscar" className="h-[36px] py-0 px-10" />
+              <Botao variante="adicionar" texto="Buscar" className="h-[36px] py-0 px-10" onClick={handleBuscar} />
             </div>
+          </Secao>
+          <Secao>
+            {
+              isLoadingDadosDaTabela &&
+              <div className="flex justify-center items-center h-40">
+                <span>Carregando...</span>
+              </div>
+            }
+            {
+              !isLoadingDadosDaTabela && dadosDaTabela &&
+              <span>...</span>
+            }
           </Secao>
         </Secao>
       </Secao>
