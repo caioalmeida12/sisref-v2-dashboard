143defb224871297eb43ff8f995fbc7a42be0ef8
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-08-28 18:14:56 -0300

    feat(FetchHelper): implementando o helper para fetch

--- src/app/(paginas)/(estudante)/page.tsx
+++ src/app/(paginas)/(estudante)/page.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { Aviso } from '@/app/elementos/modulos/Aviso';
 import { InformacoesDeEstudante } from '@/app/elementos/modulos/InformacoesDeEstudante';
 import { fetchTicketsSemJustificativa } from '@/app/actions/fetchTicketsSemJustificativa';
+import { FetchHelper } from '@/app/lib/actions/FetchHelper';
+import { cookies } from 'next/headers';
+import { IRefeicao } from '@/app/elementos/interfaces/IRefeicao';
 
 export default async function Home() {
 
-  const temTicketsSemJustificativa = await fetchTicketsSemJustificativa()
+  const ticketsSemJustificativa = await FetchHelper.get<IRefeicao>({
+    url: '/student/schedulings/not-used-without-justification',
+    cookies: cookies(),
+  })
 
   return (
     <>
       <InformacoesDeEstudante />
       {
-        temTicketsSemJustificativa.length ?
+        ticketsSemJustificativa.sucesso ?
           <Aviso titulo='Aviso' texto='Você possui um ou mais tickets pendentes de justificativa. Por favor, acesse o histórico de refeições para regularizar sua situação.' />
           : null
       }
