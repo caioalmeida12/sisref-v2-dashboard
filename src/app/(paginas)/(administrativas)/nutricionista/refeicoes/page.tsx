882e5a70cd685c51f9452bd009cd95d8b322cedd
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-09-17 13:49:42 -0300

    feat(cardapios): atualizei a pagina para seguir as praticas da pagina de agendamentos

--- src/app/(paginas)/(administrativas)/nutricionista/refeicoes/page.tsx
+++ src/app/(paginas)/(administrativas)/nutricionista/refeicoes/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import React from "react";
+import React, { useMemo } from "react";
 
 import { Secao } from "@/app/elementos/basicos/Secao";
 import { CabecalhoDeSecao } from "@/app/elementos/basicos/CabecalhoDeSecao";
 import { useQuery } from "@tanstack/react-query";
 import { TabelaDeCrud } from "@/app/elementos/modulos/comuns/TabelaDeCrud/TabelaDeCrud";
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
 import { DatasHelper } from "@/app/lib/elementos/DatasHelper";
 import { buscarRefeicoes } from "@/app/actions/nutricionista";
 import { ModalRemoverRefeicao } from "@/app/elementos/modulos/nutricionista/Refeicoes/ModalRemoverRefeicao";
@@ -25,52 +25,48 @@ export default function NutricionistaPage() {
     initialData: []
   });
 
-  const colunas = React.useMemo<ColumnDef<TRefeicao>[]>(
-    () => [
-      {
-        accessorKey: 'ID',
-        accessorFn: (row) => row?.id,
-        cell: info => info.getValue(),
-        meta: {
-          filterVariant: "range"
-        }
-      }, {
-        accessorKey: 'Nome',
-        accessorFn: (row) => row?.description,
-        cell: info => info.getValue(),
-      }, {
-        accessorKey: 'Início da refeição',
-        accessorFn: (row) => row?.timeStart,
-        cell: info => info.getValue(),
-      }, {
-        accessorKey: 'Fim da refeição',
-        accessorFn: (row) => row?.timeEnd,
-        cell: info => info.getValue(),
-      }, {
-        accessorKey: 'Horário para início das reservas',
-        accessorFn: (row) => row?.qtdTimeReservationStart,
-        cell: info => info.row.original?.timeStart ? DatasHelper.aplicarHorasEmFormatoBrasileiro(info.row.original?.timeStart, -info.row.original.qtdTimeReservationStart || 0) : 'Não informado',
-      }, {
-        accessorKey: 'Horário para fim das reservas',
-        accessorFn: (row) => row?.qtdTimeReservationEnd,
-        cell: info => info.row.original?.timeEnd ? DatasHelper.aplicarHorasEmFormatoBrasileiro(info.row.original?.timeEnd, -info.row.original.qtdTimeReservationEnd || 0) : 'Não informado',
-      },
-      {
-        header: 'Ações',
-        id: 'Ações',
-        cell: info => (
-          <div className="flex justify-center gap-x-2">
-            <div className="w-5 h-5 relative">
-              <ModalRemoverRefeicao refeicao={info.row.original!} />
-            </div>
-            <div className="w-5 h-5 relative">
-              <ModalEditarRefeicao refeicao={info.row.original} />
-            </div>
-          </div>)
-      }
-    ],
-    []
-  )
+  const colunasHelper = createColumnHelper<TRefeicao>();
+
+  const colunas = useMemo(() => [
+    colunasHelper.accessor('id', {
+      cell: props => <p>{props.getValue()}</p>,
+      header: 'ID',
+    }),
+    colunasHelper.accessor('description', {
+      cell: props => <div className="whitespace-nowrap">{props.getValue()}</div>,
+      header: 'Nome',
+    }),
+    colunasHelper.accessor('timeStart', {
+      cell: props => props.getValue(),
+      header: 'Início da refeição',
+    }),
+    colunasHelper.accessor('timeEnd', {
+      cell: props => props.getValue(),
+      header: 'Fim da refeição',
+    }),
+    colunasHelper.accessor('qtdTimeReservationStart', {
+      cell: props => props.row.original?.timeStart ? DatasHelper.aplicarHorasEmFormatoBrasileiro(props.row.original?.timeStart, -props.row.original.qtdTimeReservationStart || 0) : 'Não informado',
+      header: 'Início das reservas',
+    }),
+    colunasHelper.accessor('qtdTimeReservationEnd', {
+      cell: props => props.row.original?.timeEnd ? DatasHelper.aplicarHorasEmFormatoBrasileiro(props.row.original?.timeEnd, -props.row.original.qtdTimeReservationEnd || 0) : 'Não informado',
+      header: 'Fim das reservas',
+    }),
+    colunasHelper.display({
+      cell: props => (
+        <div className="flex justify-center gap-x-2">
+          <div className="w-5 h-5 relative">
+            <ModalRemoverRefeicao refeicao={props.row.original} />
+          </div>
+          <div className="w-5 h-5 relative">
+            <ModalEditarRefeicao refeicao={props.row.original} />
+          </div>
+        </div>
+      ),
+      enableResizing: false,
+      header: 'Ações',
+    })
+  ], []);
 
   return (
     <>
@@ -92,7 +88,7 @@ export default function NutricionistaPage() {
             {
               !isLoadingDadosDaTabela &&
               dadosDaTabela &&
-              <TabelaDeCrud colunas={colunas} dados={dadosDaTabela}/>
+              <TabelaDeCrud colunas={colunas} dados={dadosDaTabela} />
             }
           </Secao>
         </Secao>
