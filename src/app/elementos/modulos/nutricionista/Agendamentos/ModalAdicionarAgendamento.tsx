7572d0b588fef8446c245986ac0051a8b313dbe6
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-09-26 10:41:08 -0300

    refactor(modalAdicionarAgendamento): refatorei para a nova versao dos modais

--- src/app/elementos/modulos/nutricionista/Agendamentos/ModalAdicionarAgendamento.tsx
+++ src/app/elementos/modulos/nutricionista/Agendamentos/ModalAdicionarAgendamento.tsx
@@ -9,6 +9,7 @@ import * as Select from "@radix-ui/react-select";
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as Form from "@radix-ui/react-form";
 
 export const ModalAdicionarAgendamento = () => {
   const { atualizarMensagem, mensagemDeRespostaRef } = useMensagemDeResposta();
@@ -62,7 +63,7 @@ export const ModalAdicionarAgendamento = () => {
   });
 
   return (
-    <Dialog.Root open={modalAberto}>
+    <Dialog.Root open={modalAberto} onOpenChange={setModalAberto}>
       <Dialog.Trigger asChild>
         <Botao
           className="h-[36px] bg-azul-400 px-10 py-2 leading-tight"
@@ -73,50 +74,52 @@ export const ModalAdicionarAgendamento = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-preto-400/25 data-[state=open]:animate-overlayShow" />
-        <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded bg-branco-400 p-6 focus:outline-none data-[state=open]:animate-contentShow">
+        <Dialog.Content className="fixed left-[50%] top-[50%] flex max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col gap-y-2 rounded bg-branco-400 p-6 outline outline-1 outline-cinza-600 focus:outline-none data-[state=open]:animate-contentShow">
           <Dialog.Title className="m-0 text-lg font-medium">
             Adicionar agendamento
           </Dialog.Title>
           <Dialog.Description>
             Preencha os campos abaixo para adicionar um novo agendamento.
           </Dialog.Description>
-          <form
+          <hr />
+          <Form.Root
             className="flex flex-col gap-y-4"
             onSubmit={(e) => {
               e.preventDefault();
               handleSalvar(new FormData(e.target as HTMLFormElement));
             }}
           >
-            <fieldset className="flex flex-col justify-start gap-y-2">
-              <label className="font-medium" htmlFor="student_id">
+            <Form.Field name="student_id" className="flex flex-col gap-y-1">
+              <Form.Label className="font-medium" htmlFor="student_id">
                 Código de estudante
-              </label>
-              <input
-                className="inline-flex h-8 w-full flex-1 items-center justify-center rounded-[4px] px-4 py-2 leading-none shadow-[0_0_0_1px] shadow-preto-400 outline-none focus:shadow-[0_0_0_2px] focus:shadow-preto-400"
+              </Form.Label>
+              <Form.Control
+                className="h-[34px] w-full rounded px-2 py-1 outline outline-1"
                 id="student_id"
                 placeholder="Ex: 2153 (código do restaurante)"
                 name="student_id"
                 type="number"
               />
-            </fieldset>
-            <fieldset className="flex flex-col justify-start gap-y-2">
-              <label className="font-medium" htmlFor="date">
+            </Form.Field>
+            <Form.Field name="date" className="flex flex-col gap-y-1">
+              <Form.Label className="font-medium" htmlFor="date">
                 Data
-              </label>
-              <div className="rounded outline outline-1">
-                <input
-                  type="date"
-                  id="date"
-                  name="date"
-                  className="w-full bg-cinza-400 px-2 py-1"
-                  defaultValue={DatasHelper.getDataDeHoje()}
-                />
-              </div>
-            </fieldset>
-            <fieldset className="flex flex-col justify-start gap-y-2">
-              <label className="font-medium" htmlFor="meal_id">
+              </Form.Label>
+              <Form.Control
+                type="date"
+                id="date"
+                name="date"
+                className="w-full rounded px-2 py-1 outline outline-1"
+                defaultValue={DatasHelper.getDataDeHoje()}
+              />
+            </Form.Field>
+            <Form.Field
+              name="meal_id"
+              className="flex flex-col justify-start gap-y-1"
+            >
+              <Form.Label className="font-medium" htmlFor="meal_id">
                 Refeição
-              </label>
+              </Form.Label>
               <Select.Root name="meal_id">
                 <Select.Trigger className="flex h-[34px] min-w-[250px] items-center overflow-hidden rounded px-2 py-1 text-left outline outline-1 outline-preto-400">
                   <Select.Value placeholder="Selecione a refeição desejada" />
@@ -151,7 +154,7 @@ export const ModalAdicionarAgendamento = () => {
                   </Select.Content>
                 </Select.Portal>
               </Select.Root>
-            </fieldset>
+            </Form.Field>
             <div className="flex flex-col items-center justify-end gap-y-2">
               <div className="text-center" ref={mensagemDeRespostaRef}></div>
               <Botao
@@ -172,8 +175,7 @@ export const ModalAdicionarAgendamento = () => {
                 <Cross2Icon />
               </button>
             </Dialog.Close>
-            <input name="meal_id" />
-          </form>
+          </Form.Root>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
