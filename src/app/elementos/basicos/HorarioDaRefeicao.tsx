8573900f173b616d5de13d65af6bbbeab58b9b26
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-10-02 08:52:42 -0300

    refactor(horarioDaRefeicao): utilizando os horarios que vem da api

--- src/app/elementos/basicos/HorarioDaRefeicao.tsx
+++ src/app/elementos/basicos/HorarioDaRefeicao.tsx
@@ -9,14 +9,17 @@ import classnames from "classnames";
 
 type HorarioDaRefeicaoProps = IApenasHorario | IApenasData | IHorarioEData;
 
+// Recebe uma string no formato "HH:MM:SS" e retorna "HH:MM"
+const removerSegundos = (horario: string) => horario.slice(0, 5);
+
 const textoPorVariante = (props: HorarioDaRefeicaoProps) => {
   switch (props.variante) {
     case "horario":
-      return `${props.horarios.timeStart}h às ${props.horarios.timeEnd}h`;
+      return `${removerSegundos(props.horarios.timeStart)}h às ${removerSegundos(props.horarios.timeEnd)}h`;
     case "data":
-      return `${props.data}`;
+      return `${removerSegundos(props.data)}`;
     case "horario-e-data":
-      return `${props.data} - ${props.horarios.timeStart}h às ${props.horarios.timeEnd}h`;
+      return `${removerSegundos(props.data)} - ${removerSegundos(props.horarios.timeStart)}h às ${removerSegundos(props.horarios.timeEnd)}h`;
   }
 };
 
