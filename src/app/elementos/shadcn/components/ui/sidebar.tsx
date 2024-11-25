6b7e5c4cb51787d47e422f7edc4fc880df19d208
caioalmeida12 <araujo.caio08@aluno.ifce.edu.br>
2024-11-25 08:50:56 -0300

    fix(sidebar): removi o outline

--- src/app/elementos/shadcn/components/ui/sidebar.tsx
+++ src/app/elementos/shadcn/components/ui/sidebar.tsx
@@ -223,7 +223,7 @@ const Sidebar = React.forwardRef<
             <VisuallyHidden>
               <SheetTitle>Menu</SheetTitle>
             </VisuallyHidden>
-            <div className="flex h-full w-full flex-col">{children}</div>
+            <div className="flex h-full w-full flex-col" id="sidebar-shadcn">{children}</div>
           </SheetContent>
         </Sheet>
       );
@@ -631,7 +631,7 @@ const SidebarMenuAction = React.forwardRef<
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
+        "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
         className,
       )}
       {...props}
