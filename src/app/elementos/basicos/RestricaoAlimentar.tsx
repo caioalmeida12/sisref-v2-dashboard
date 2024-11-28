e12ff8e5bebd5afbb8e16deb12b456fe213473c7
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-11-28 09:48:23 -0300

    chore(restricoesAlimentares): removi o mock

--- src/app/elementos/basicos/RestricaoAlimentar.tsx
+++ src/app/elementos/basicos/RestricaoAlimentar.tsx
@@ -17,14 +17,16 @@ export const RestricaoAlimentar = ({
   return (
     <div className="item-center relative flex justify-between rounded border-[1px] border-cinza-600 p-4">
       <div className="relative flex items-center gap-2">
-        <Icone.RestricaoAlimentar />
+        <div className="[&>svg>path]:fill-preto-400">
+          <Icone.RestricaoAlimentar />
+        </div>
         <p className="text-sm font-bold leading-4 text-preto-400">{texto}</p>
       </div>
-      <div className="relative flex items-center gap-2 text-vermelho-400 hover:font-medium">
+      {/* <div className="relative flex items-center gap-2 text-vermelho-400 hover:font-medium">
         <button name="Remover" onClick={onRemove} className="cursor-pointer">
           Remover
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
