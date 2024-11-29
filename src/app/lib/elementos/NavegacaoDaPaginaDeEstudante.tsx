054a4c06bfbc7a6bde9c1247822955396cf9c7e5
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-11-29 08:30:59 -0300

    chore(estudante): tornei a navegacao mobile padrao

--- src/app/lib/elementos/NavegacaoDaPaginaDeEstudante.tsx
+++ src/app/lib/elementos/NavegacaoDaPaginaDeEstudante.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 
 import { parseAsBoolean, parseAsString, useQueryStates } from "nuqs";
 
 export const useNavegacaoDaPaginaDeEstudante = () => {
-    return useQueryStates({
-        pagina: parseAsString.withDefault("refeicoesPorDia"),
-        isMobile: parseAsBoolean.withDefault(false)
-    }, {
-        clearOnDefault: true
-    });
-}
+  return useQueryStates(
+    {
+      pagina: parseAsString.withDefault("refeicoesPorDia"),
+      isMobile: parseAsBoolean.withDefault(false),
+    },
+    {
+      clearOnDefault: true,
+    },
+  );
+};
