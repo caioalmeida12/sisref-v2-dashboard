5473e4132676b02c461b5816712e73f8fac36e51
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2024-11-29 08:34:51 -0300

    chore(restricoesAlimentares): removi o restante do mock

--- src/app/elementos/modulos/estudante/RestricoesAlimentares/RestricoesAlimentares.tsx
+++ src/app/elementos/modulos/estudante/RestricoesAlimentares/RestricoesAlimentares.tsx
@@ -1,58 +1,20 @@
 "use client";
 
-import React from "react";
-
 import { CabecalhoDeSecao } from "@elementos/basicos/CabecalhoDeSecao";
-import { RestricaoAlimentar } from "@elementos/basicos/RestricaoAlimentar";
 import { Secao } from "@elementos/basicos/Secao";
-import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import { TextoDescritivo } from "./TextoDescritivo";
 
-const TEMPO_DE_CARREGAMENTO = 2000;
-
 export const RestricoesAlimentares = () => {
-  const [restricoes, setRestricoes] = useState<string[]>([]);
-  const [carregando, setCarregando] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      // setRestricoes(["Glúten", "Lactose", "Frutos do mar"]);
-
-      setCarregando(false);
-    }, TEMPO_DE_CARREGAMENTO);
-  }, []);
-
-  const handleRemoverRestricao = (e: React.MouseEvent) => {
-    const restricao =
-      e.currentTarget?.parentElement?.previousSibling?.textContent;
-    if (!restricao) return;
-
-    setRestricoes(restricoes.filter((item) => item !== restricao));
-  };
-
   return (
     <div className={`flex flex-col gap-4 lg:flex`} id="restricoesAlimentares">
       <TextoDescritivo />
       <Secao className="flex flex-col gap-4">
         <CabecalhoDeSecao titulo="Suas restrições alimentares" />
-        {carregando ? (
-          <LoadingSkeletons />
-        ) : (
-          restricoes.map((item, index) => (
-            <RestricaoAlimentar
-              key={index}
-              texto={item}
-              onRemove={handleRemoverRestricao}
-            />
-          ))
-        )}
-        {restricoes.length == 0 ? (
-          <p className="text-justify">
-            Você não possui restrições alimentares cadastradas. Caso queira
-            adicionar alguma, entre em contato com a pessoa nutricionista.
-          </p>
-        ) : null}
+        <p className="text-justify">
+          Você não possui restrições alimentares cadastradas. Caso queira
+          adicionar alguma, entre em contato com a pessoa nutricionista.
+        </p>
         {/* <Botao
           texto="Adicionar"
           variante="adicionar"
@@ -61,10 +23,4 @@ export const RestricoesAlimentares = () => {
       </Secao>
     </div>
   );
-};
-
-const LoadingSkeletons = () => {
-  return Array.from({ length: 3 }).map((_, index) => (
-    <Skeleton key={index} height={58} />
-  ));
 };
