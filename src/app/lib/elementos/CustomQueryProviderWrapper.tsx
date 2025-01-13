8ca9ded086037b25a5ebb088b2b040b362ff7d3f
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2025-01-13 16:22:21 -0300

    fix(ModalEditarEstudante): corrigir a busca por turnos

--- src/app/lib/elementos/CustomQueryProviderWrapper.tsx
+++ src/app/lib/elementos/CustomQueryProviderWrapper.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./QueryClient";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const CustomQueryClientProvider = ({
   children,
@@ -9,6 +10,9 @@ export const CustomQueryClientProvider = ({
   children: React.ReactNode;
 }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   );
 };
