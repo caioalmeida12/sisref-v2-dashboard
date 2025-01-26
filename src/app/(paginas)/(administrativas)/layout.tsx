b796a13e9b21b0dac4adaae9e2d26fd60355a29f
Caio de Almeida Araujo <araujo.caio08@aluno.ifce.edu.br>
2025-01-26 14:39:01 -0300

    temp: adicionar debug do react-query e react-scan

--- src/app/(paginas)/(administrativas)/layout.tsx
+++ src/app/(paginas)/(administrativas)/layout.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from "@/app/elementos/modulos/comuns/Sidebar";
 import { CustomQueryClientProvider } from "@/app/lib/elementos/CustomQueryProviderWrapper";
 // import { validarTokenDosCookies } from "@/app/lib/middlewares/ValidarTokenDosCookies";
 import React, { Suspense } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface NutricionistaLayoutProps {
   children: React.ReactNode;
@@ -19,6 +20,7 @@ export default async function NutricionistaLayout({
         <div className="max-h-screen w-full overflow-y-auto p-8">
           <Suspense fallback={<div>Carregando...</div>}>{children}</Suspense>
         </div>
+        <ReactQueryDevtools initialIsOpen={false} />
       </div>
     </CustomQueryClientProvider>
   );
